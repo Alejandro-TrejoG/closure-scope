a7c0c45a956791bf9713333ee8c9cea3c3b1226c
Alejandro Trejo Godinez <alejandro.trejog.15h@gmail.com>
2022-02-16 10:17:27 -0600

    Debugger

---
+++ src/debug/index.js
@@ -0,0 +1,14 @@
+var a = "Hello"
+
+function hello() {
+    let b = "Hello World"
+    const c = "Hello World from C"
+    if (true) {
+        let d = "Hello World from D"
+
+        // debugger nos permite pausar la ejecucion del codigo para poder observar como esta trabajando
+        debugger
+    }
+}
+
+hello()
